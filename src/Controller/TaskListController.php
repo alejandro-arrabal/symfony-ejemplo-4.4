41b60cc4e77aa064e363569f703c15472b6579ad
alejandro.arrabal <alejandro.arrabal@sngular.com>
2020-05-04 16:59:57 +0200

    Fix on controllers

--- src/Controller/TaskListController.php
+++ src/Controller/TaskListController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Task;
 use App\Repository\TaskListRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class CollaboratorController
  * @Route("/api/list")
  */
-class TaskListController extends FOSRestController
+class TaskListController extends AbstractFOSRestController
 {
     /**
      * @Rest\Get("/{id}", name="get_task")
